40e4ad3e4a7389b3c1bfe8a6a86e01e55834e0e8
Leonardus <LBotes@yellowtail.nl>
2023-11-02 20:20:12 +0200

    added a few components

--- resources/views/components-page.blade.php
+++ resources/views/components-page.blade.php
@@ -19,6 +19,38 @@
 
     <div class="py-12 max-w-7xl mx-auto sm:px-6 lg:px-8">
         <h2 class="font-semibold mb-4">Form</h2>
-        <x-form title="This is my title"></x-form>
+        <x-form title="This is my title">
+            <div>
+                <x-input-label for="password" value="{{ __('Password') }}" class="sr-only" />
+
+                <x-text-input id="password" name="password" type="password" class="mt-1 block w-full"
+                    placeholder="{{ __('Password') }}" />
+
+                <x-input-error :messages="$errors->userDeletion->get('password')" class="mt-2" />
+            </div>
+
+            <div>
+                <x-input-label for="password" value="{{ __('Password') }}" class="sr-only" />
+
+                <x-text-input id="password" name="password" type="password" class="mt-1 block w-full"
+                    placeholder="{{ __('Password') }}" />
+
+                <x-input-error :messages="$errors->userDeletion->get('password')" class="mt-2" />
+            </div>
+
+            <div>
+                <x-input-label for="password" value="{{ __('Password') }}" class="sr-only" />
+
+                <x-text-input id="password" name="password" type="password" class="mt-1 block w-full"
+                    placeholder="{{ __('Password') }}" />
+
+                <x-input-error :messages="$errors->userDeletion->get('password')" class="mt-2" />
+            </div>
+        </x-form>
+    </div>
+
+    <div class="py-12 max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <h2 class="font-semibold mb-4">Carousel</h2>
+        <x-carousel></x-carousel>
     </div>
 </x-app-layout>
--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -1,67 +1,22 @@
-@props([
-    'title'
-])
+@props(['title'])
 
 <div>
-<x-primary-button
-        x-data=""
-        x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')"
-    >{{ __('Open Modal') }}</x-primary-button>
+    <x-primary-button x-data=""
+        x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')">{{ __('Open Modal') }}</x-primary-button>
 
     <x-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
         <form method="post" action="{{ route('profile.destroy') }}">
             @csrf
             @method('delete')
 
-            <h2 class="text-xl font-medium text-gray-900 dark:text-gray-100 border-b-2 border-gray-200 p-4 px-6">
+            <h2 class="text-xl font-medium text-white dark:text-gray-100 border-b-2 border-gray-200 p-4 px-6 bg-gray-700 dark:bg-gray-700">
                 {{ $title }}
             </h2>
 
             <div class="p-6 flex flex-col gap-6">
 
-            <div>
-                <x-input-label for="password" value="{{ __('Password') }}" class="sr-only" />
+                {{$slot}}
 
-                <x-text-input
-                    id="password"
-                    name="password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    placeholder="{{ __('Password') }}"
-                />
-
-                <x-input-error :messages="$errors->userDeletion->get('password')" class="mt-2" />
-            </div>
-
-            <div>
-                <x-input-label for="password" value="{{ __('Password') }}" class="sr-only" />
-
-                <x-text-input
-                    id="password"
-                    name="password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    placeholder="{{ __('Password') }}"
-                />
-
-                <x-input-error :messages="$errors->userDeletion->get('password')" class="mt-2" />
-            </div>
-
-            <div>
-                <x-input-label for="password" value="{{ __('Password') }}" class="sr-only" />
-
-                <x-text-input
-                    id="password"
-                    name="password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    placeholder="{{ __('Password') }}"
-                />
-
-                <x-input-error :messages="$errors->userDeletion->get('password')" class="mt-2" />
-            </div>
-
-            
             </div>
 
             <div class="px-6 pt-4 flex justify-end border-t-2 border-gray-200">
